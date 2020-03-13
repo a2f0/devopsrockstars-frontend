2ebba879b26b6e4880cc79f14ff2b08908f04a5c
Dan Sullivan <dansullivan@gmail.com>
2020-03-13 15:36:37 -0500

    implemented leaflet

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import './style/map.less';
 import './style/devopsrockstars.less';
 import skylineUrl from './image/chicago_skyline.svg';
+import { Map, TileLayer } from 'react-leaflet'
+import { useLocation } from "react-router-dom";
 
 
 function Index() {
@@ -98,6 +101,42 @@ function Contact() {
   return <h2>Contact</h2>;
 }
 
+function FullScreenMap() {
+  
+  const getClassname = () => { 
+    const location = useLocation();
+    switch(location.pathname) { 
+    case '/contact': 
+      return 'visible';  
+    default:
+      return 'hidden';
+    } 
+  }
+
+  return (
+    <div id='mapdiv' className={getClassname()}>
+      <link
+        rel="stylesheet"
+        href="//cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+      />
+      <link 
+        rel="stylesheet" 
+        href="//unpkg.com/leaflet@1.6.0/dist/leaflet.css"
+      />
+      <Map 
+        attributionControl={false}
+        center={[45.4, -75.7]} 
+        zoom={12} 
+        zoomControl={false}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap2</a> contributors'
+        />
+      </Map>   
+    </div>
+  )
+}
+
 function NavigationTop() {
   return (
     <div className="navbar navbar-top">
@@ -114,13 +153,13 @@ function NavigationTop() {
             <div className="flex">
               <div className="flex-container-right">
                 <div className="menu-item">
-                  <Link to="/company/">Kiva</Link>
+                  <Link to="/company">Kiva</Link>
                 </div>
                 <div className="menu-item">
-                  <Link to="/store/">Store</Link>
+                  <Link to="/store">Store</Link>
                 </div>
                 <div className="menu-item">
-                  <Link to="/contact/">Contact</Link>
+                  <Link to="/contact">Contact</Link>
                 </div>
               </div>
             </div>
@@ -172,12 +211,13 @@ function AppRouter() {
   return (
     <Router>
       <NavigationTop />
+      <FullScreenMap/>
       <div className="flex-container">
         <div className="flex-max-width">
           <Route path="/" exact component={Index} />
-          <Route path="/company/" exact component={Company} />
-          <Route path="/store/" component={Store} />
-          <Route path="/contact/" component={Contact} />
+          <Route path="/company" exact component={Company} />
+          <Route path="/store" component={Store} />
+          <Route path="/contact" component={Contact} />
         </div>
       </div>
       <NavigationBottom />
