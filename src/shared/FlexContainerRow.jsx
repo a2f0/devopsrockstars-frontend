2648699989a6422a4f47ee3a91e2031796ea3b3f
Dan Sullivan <dansullivan@gmail.com>
2020-09-01 18:53:33 -0400

    install material ui abstract flex container row

---
+++ src/shared/FlexContainerRow.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => ({	
+  flexContainerRow: {	
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    width: '100%'
+  },
+}));
+
+const FlexContainerRow = function (props) {
+  const classes = useStyles();
+  return (
+    <div
+      className={classes.flexContainerRow}>
+      {props.children}
+    </div>
+  )
+}
+export default FlexContainerRow
