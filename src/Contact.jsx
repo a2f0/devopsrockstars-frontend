38149bd47c50f4214e4398c214159b5065f2022f
Dan Sullivan <dansullivan@gmail.com>
2020-09-01 20:52:07 -0400

    abstract store and contact

---
+++ src/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = React.memo(function Contact(props) {
+
+  return (
+    <h2>Contact</h2>
+  );
+
+});
+
+export default Contact
---
+++ src/Store.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Store = React.memo(function Store(props) {
+
+  return (
+    <h2>Store</h2>
+  );
+
+});
+
+export default Store
