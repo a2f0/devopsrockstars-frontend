bb3a43a33b57238ae1a22962f3ef28b9f7b1575e
Dan Sullivan <dansullivan@gmail.com>
2020-09-01 20:52:07 -0400

    abstract store and contact

--- src/index.js
+++ src/index.js
@@ -8,14 +8,8 @@ import FullScreenMap from "./FullScreenMap.jsx";
 import Company from "./Company.jsx";
 import Skyline from "./Skyline.jsx";
 import FlexContainerRow from "./shared/FlexContainerRow.jsx"
-
-function Store() {
-  return <h2>Store</h2>;
-}
-
-function Contact() {
-  return <h2>Contact</h2>;
-}
+import Store from "./Store.jsx"
+import Contact from "./Contact.jsx"
 
 function AppRouter() {
   return (
