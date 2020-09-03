b75052ed3f3f18d9ab9b4993cf368ea98e7a3e73
Dan Sullivan <dansullivan@gmail.com>
2020-09-03 13:43:28 -0400

    retrieve store products

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,22 +1,33 @@
-import React, {useEffect} from 'react'
+import React, {useEffect, useState} from 'react'
 import { StoreProduct } from "./models"
 
 const Store = React.memo(function Store() {
 
+  const [storeProducts, setStoreProducts] = useState([])
+
   useEffect(() => {
     retrieveEntries();
-  });
+  }, []);
 
   const retrieveEntries = () => {
     const fetchData = async () => {
-      StoreProduct.all()
+      let response = await StoreProduct.includes('store_product_pictures').all()
+      setStoreProducts(response.data)
     }
     fetchData();
   }
 
 
   return (
-    <h2>Store</h2>
+    <>
+      <h2>Store</h2>
+      {storeProducts.map(function(product, index){
+        return (
+          <div key={'product' + product.id}>
+          </div>
+        )
+      })}
+    </>
   );
 
 });
