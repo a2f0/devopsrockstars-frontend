c4c1e5eaa2fc0d81c6d9f3f2592e008b66bd5870
Dan Sullivan <dansullivan@gmail.com>
2020-09-06 01:18:18 -0400

    download store products and pictures

--- src/models.tsx
+++ src/models.tsx
@@ -2,16 +2,34 @@ import {
   Model,
   SpraypaintBase,
   Attr,
+  HasMany,
+  BelongsTo,
 } from "spraypaint"
+
+export const baseHTTPURL = () => {
+  if (window.location.hostname === 'esor.devopsrockstars.com') {
+    return 'https://rose.devopsrockstars.com';
+  } else {
+    return 'http://localhost:3000';
+  }
+}
+
 
 @Model()
 class ApplicationRecord extends SpraypaintBase {
-  static baseUrl = "http://localhost:3000"
+  static baseUrl = baseHTTPURL()
   static apiNamespace = "/api/v1"
+}
+
+@Model()
+export class StoreProductPicture extends ApplicationRecord {
+  static jsonapiType = "store_product_pictures"
+  @Attr url: string
+  @BelongsTo() storeProduct: StoreProduct
 }
 
 @Model()
 export class StoreProduct extends ApplicationRecord {
   static jsonapiType = "store_products"
-  @Attr() firstName: string
+  @HasMany() storeProductPictures: StoreProductPicture[]
 }
