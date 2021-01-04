3d2148ee55e2eff939b12a942a1966f98eeab86e
Dan Sullivan <dansullivan@gmail.com>
2021-01-04 13:20:19 -0500

    set map bounds and min zoom

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
 import { Map, TileLayer } from 'react-leaflet'
+import L from 'leaflet';
 
 const FullScreenMap = React.memo(function FullScreenMap(props) {
 
@@ -13,6 +14,11 @@ const FullScreenMap = React.memo(function FullScreenMap(props) {
       return 'hidden';
     } 
   }
+
+
+  const southWest = L.latLng(17.476, -126.386);
+  const northEast = L.latLng(51.289, -44.472);
+  const bounds = L.latLngBounds(southWest, northEast);
 
   return (
     <div id='mapdiv' className={`full-screen ${getClassname()}`}>
@@ -29,7 +35,9 @@ const FullScreenMap = React.memo(function FullScreenMap(props) {
         attributionControl={false}
         center={[41.796242, -87.580913]} 
         zoom={11} 
-        zoomControl={false}>
+        minZoom={5}
+        zoomControl={false}
+        maxBounds={bounds}>
         <TileLayer
           url="https://api.mapbox.com/styles/v1/devopsrockstars/ckhobopaz1o8519lupxgprhji/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGV2b3Bzcm9ja3N0YXJzIiwiYSI6InpUN3Buak0ifQ.a9f4FaZbPCK6GNJc8ImH7w"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap2</a> contributors'
