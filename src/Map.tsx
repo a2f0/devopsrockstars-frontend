a42102815840f311d51f3b27947eaf43ca57cd1b
a2f0 <dansullivan@gmail.com>
2021-09-11 01:33:33 -0400

    fix gaps / background in rendered map tiles (#20)

--- src/Map.tsx
+++ src/Map.tsx
@@ -22,20 +22,18 @@ const FullScreenMap = React.memo(function FullScreenMap(props) {
 
   return (
     <div id='mapdiv' className={`full-screen-map ${getClassname()}`}>
-      <link
-        rel="stylesheet"
-        href="//cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
-      />
-      <link
-        rel="stylesheet"
-        href="//unpkg.com/leaflet@1.6.0/dist/leaflet.css"
-      />
+      <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A==" crossOrigin=""/>
+      <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+        integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+        crossOrigin=""></script>
       {/* https://docs.mapbox.com/studio-manual/overview/publish-your-style/#mapboxjs-and-leaflet */}
       <MapContainer
         attributionControl={false}
         center={[41.796242, -87.580913]}
         zoom={11}
         minZoom={5}
+        zoomSnap={1}
         zoomControl={false}
         maxBounds={bounds}>
         <TileLayer
