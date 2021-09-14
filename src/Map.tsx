751b4c30f9ac2e0193e78bfd8cf9b58b4c98d213
a2f0 <dansullivan@gmail.com>
2021-09-14 01:10:43 -0400

    Fix/map marker (#25)
    * update map marker
    * fix broken test

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { useLocation } from "react-router-dom";
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker} from 'react-leaflet'
 import L from 'leaflet';
+import whiteStar from '/static/image/white-star-only.svg';
 
 const FullScreenMap = React.memo(function FullScreenMap(props) {
 
@@ -20,6 +21,14 @@ const FullScreenMap = React.memo(function FullScreenMap(props) {
   const northEast = L.latLng(51.289, -44.472);
   const bounds = L.latLngBounds(southWest, northEast);
 
+  let starIcon = L.icon({
+    iconUrl: whiteStar,
+    iconRetinaUrl: whiteStar,
+    iconAnchor: [5, 55],
+    popupAnchor: [10, -44],
+    iconSize: [25, 55],
+  });
+
   return (
     <div id='mapdiv' className={`full-screen-map ${getClassname()}`}>
       <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
@@ -30,8 +39,8 @@ const FullScreenMap = React.memo(function FullScreenMap(props) {
       {/* https://docs.mapbox.com/studio-manual/overview/publish-your-style/#mapboxjs-and-leaflet */}
       <MapContainer
         attributionControl={false}
-        center={[41.796242, -87.580913]}
-        zoom={11}
+        center={[40.762189, -73.961183]}
+        zoom={13}
         minZoom={5}
         zoomSnap={1}
         zoomControl={false}
@@ -40,7 +49,10 @@ const FullScreenMap = React.memo(function FullScreenMap(props) {
           url="https://api.mapbox.com/styles/v1/devopsrockstars/ckhobopaz1o8519lupxgprhji/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZGV2b3Bzcm9ja3N0YXJzIiwiYSI6InpUN3Buak0ifQ.a9f4FaZbPCK6GNJc8ImH7w"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap2</a> contributors'
         />
+        <Marker position={[40.762189, -73.961183]} icon={starIcon}></Marker>
       </MapContainer>
+
+
     </div>
   )
 });
