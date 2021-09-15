7f2ccc77215614daf825d375fa5f7ab62f567f8b
a2f0 <dansullivan@gmail.com>
2021-09-15 04:42:37 -0400

    add azure tag (#32)

--- src/Company.tsx
+++ src/Company.tsx
@@ -11,15 +11,16 @@ const Company = React.memo(function Company(props) {
       <h1>
         About Us
       </h1>
-
       <p>
         We are a consultancy offering services in DevOps and development.
       </p>
-
       <h1>
         Technologies
       </h1>
-      <SVG height={100} width={100} />
+      <div>
+        <SVG height={100} width={100} path={'/static/image/tags/aws.svg'} />
+        <SVG height={100} width={100} path={'/static/image/tags/azure2.svg'} />
+      </div>
     </>
   );
 });
