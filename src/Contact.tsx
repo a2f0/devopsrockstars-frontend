fca2cce9795e26906d48a7c0ef24c16844c59e64
a2f0 <dansullivan@gmail.com>
2021-09-16 05:19:25 -0400

    contact links (#34)

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,9 +1,41 @@
 import React from 'react'
+import ContactRow from './ContactRow';
+import styled from 'styled-components';
+
+export const ContactRowContainer = styled.div`
+  margin-top: 20px;
+`;
 
 const Contact = React.memo(function Contact(props) {
 
   return (
-    <h2>Contact</h2>
+    <>
+      <h1>Contact</h1>
+      <p>
+      We are in New York, NY.
+      </p>
+      <ContactRowContainer>
+        <ContactRow
+          svgHeight={20}
+          svgWidth={20}
+          svgPath={'/static/image/contact/email.svg'}
+          uri={'mailto:info@devopsrockstars.com'}
+          rowDescription={'info [at] devopsrockstars.com'}/>
+        <ContactRow
+          svgHeight={20}
+          svgWidth={20}
+          svgPath={'/static/image/contact/vcard.svg'}
+          uri={'/static/contact/devopsrockstars.vcf'}
+          rowDescription={'vCard'}/>
+        <ContactRow
+          svgHeight={20}
+          svgWidth={20}
+          svgPath={'/static/image/contact/key.svg'}
+          uri={'/static/contact/pgp.asc'}
+          rowDescription={'GnuPG'}/>
+      </ContactRowContainer>
+
+    </>
   );
 
 });
