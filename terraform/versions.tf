4a5d725c8fbdc0c1ad08a5e4d9dac62f37101a86
a2f0 <dansullivan@gmail.com>
2021-11-01 02:19:06 -0400

    upgrade terraform (#71)

---
+++ terraform/versions.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    vercel = {
+      source  = "chronark/vercel"
+      version = "0.14.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "4.17.0"
+    }
+  }
+  required_version = ">= 1.0.10"
+}
