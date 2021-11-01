2ff9514aebe97bf405c48ac90110b5c4e1036b59
Dan Sullivan <dansullivan@gmail.com>
2021-10-31 20:05:20 -0400

    test for skyline and tech tag

--- src/Skyline.tsx
+++ src/Skyline.tsx
@@ -4,7 +4,12 @@ import skyline from '/static/image/skyline.svg';
 const Skyline = React.memo(() => {
   return (
     <div className="full-screen-skyline">
-      <img className="fill-container" src={skyline} alt="skyline" />
+      <img
+        className="fill-container"
+        src={skyline}
+        alt="skyline"
+        id="skyline"
+      />
     </div>
   );
 });
