f8e5234e99373afa2e5f84106963932e4bbbe6d9
a2f0 <dansullivan@gmail.com>
2021-10-31 22:43:12 -0400

    style skyline (#65)

--- src/Skyline.tsx
+++ src/Skyline.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import skyline from '/static/image/skyline.svg';
+import styled from 'styled-components';
+
+const FullScreenSkyline = styled.div`
+  z-index: -1337;
+  position: fixed;
+  left: 0px;
+  right: 0px;
+  width: 100vw;
+  height: 100vh;
+  align-items: flex-end;
+  display: flex;
+`;
+
+const FillContainerImg = styled.img`
+  width: 100%;
+  display: block;
+`;
 
 const Skyline = React.memo(() => {
   return (
-    <div className="full-screen-skyline">
-      <img
-        className="fill-container"
-        src={skyline}
-        alt="skyline"
-        id="skyline"
-      />
-    </div>
+    <FullScreenSkyline>
+      <FillContainerImg src={skyline} alt="skyline" id="skyline" />
+    </FullScreenSkyline>
   );
 });
 
